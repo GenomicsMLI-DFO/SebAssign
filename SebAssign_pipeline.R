83f983e579a1e38c4c4798ea1489222433cd50cb
Audrey Bourret <audrey.bourret@usherbrooke.ca>
2021-09-02 12:15:47 -0400

    Clean pipeline #2 #1

--- SebAssign_pipeline.R
+++ SebAssign_pipeline.R
@@ -1,6 +1,6 @@
 # Info --------------------------------------------------------------------
 
-# Pipeline developed to provide species diagnostic for Sebastes mentella 
+# Pipeline developed to provide species assignment for Sebastes mentella 
 # and S. fasciatus, using 4 microsatelittes loci 
 # 
 # Write by: Audrey Bourret
@@ -11,7 +11,7 @@
 
 # Library -----------------------------------------------------------------
 
-# install.packages(c("assignPOP", "readxl", "dplyr", "here", "magrittr", "tidyr", "stringr"))
+# install.packages(c("assignPOP", "readxl", "dplyr", "here", "magrittr", "tidyr", "stringr", "ggplot2"))
 
 # POPassign 
 library(assignPOP)
@@ -23,13 +23,11 @@ library(tidyr)
 library(here)
 library(magrittr)
 library(stringr)
-
-if(!require(klaR)){ install.packages("klaR") }
-library(klaR)
+library(ggplot2)
 
 # Load internal functions
-for(i in 1:length( list.files("./04_Functions") )){
-  source(file.path("./04_Functions",  list.files("./04_Functions")[i]))  
+for(i in 1:length( list.files("./03_Functions") )){
+  source(file.path("./03_Functions",  list.files("./03_Functions")[i]))  
 }
 
 
@@ -38,8 +36,11 @@ for(i in 1:length( list.files("./04_Functions") )){
 # Set these parameters
 
 locus <- c("SEB25", "SEB31", "SEB33", "SEB9")
+threshold <- 95
+   
+list.files(file.path(here::here(), "02_Data_to_Assign"))
+assign.excel <- "RAD2019_sebastes_Test.xlsx"
 
-assign.excel <- "TEL15_Coh16_BIN-MUX1-ASSIG_final_2019.xlsx"
 
 # Check if these parameters are OK
 # Name of the folder where results will be saved
@@ -87,107 +88,31 @@ if(file.exists(assign.dir)){
 # 1.Perform assignment test using genetic data and naive Bayes
 assign.X( x1=ref.gen, x2=assign.gen, dir=paste0(file.path(assign.dir,"naiveBayes"),"/"), model="naiveBayes", mplot = T)
 
-# 2.Perform assignment test using decision tree
-assign.X( x1=ref.gen, x2=assign.gen, dir=paste0(file.path(assign.dir,"tree"),"/"),  model="tree", mplot = T)
-
-# 3.Perform assignment test using random forest
-assign.X( x1=ref.gen, x2=assign.gen, dir=paste0(file.path(assign.dir,"randomForest"),"/"),  model="randomForest", mplot = T)
-
-# 4.Perform assignment test using SVM
-assign.X( x1=ref.gen, x2=assign.gen, dir=paste0(file.path(assign.dir,"svm"),"/"),  model="svm", mplot = T)
-
-
-membership.plot(dir = paste0(file.path(assign.dir,"tree"),"/"))
-
 
 # Create final file -------------------------------------------------------
 
-res1 <- read.table(file.path(assign.dir,"naiveBayes", "AssignmentResult.txt"), header = T)
-res1$Model <- "naiveBayes"
+res <- read.table(file.path(assign.dir,"naiveBayes", "AssignmentResult.txt"), header = T)
 
-res2 <- read.table(file.path(assign.dir,"tree", "AssignmentResult.txt"), header = T)
-res2$Model <- "tree"
+res <- res %>%  mutate(SP.95 = ifelse(pred.pop == "fasciatus" & fasciatus >= 0.95, "fasciatus",
+                               ifelse(pred.pop == "mentella_golf" & mentella_golf >= 0.95, "mentella_golf",
+                                          "undetermined")) ) %>%
+                left_join(assign.df %>% select(ID, POP), by = c("Ind.ID" = "ID") ) %>% 
+                transmute(Ind.ID, POP, SP.95, Prob.fasciatus = fasciatus, Prob.mentella_golf = mentella_golf)
 
-res3 <- read.table(file.path(assign.dir,"randomForest", "AssignmentResult.txt"), header = T)
-res3$Model <- "randomForest"
+res
 
-res4 <- read.table(file.path(assign.dir,"svm", "AssignmentResult.txt"), header = T)
-res4$Model <- "svm"
-
-res <- rbind(res1, res2, res3, res4)
+write.csv2(res, file.path(assign.dir, paste0("Results_assignments_", Sys.Date(), ".csv")), row.names = F)
 
 
-res %>% ggplot(aes(x=pop.1)) + 
-               geom_histogram() + 
-               geom_vline(xintercept = c(0.05, 0.95), col = "blue") +
-               geom_vline(xintercept = c(0.10, 0.90), col = "red") +
-   facet_wrap(~Model) +            
-   theme_bw()
+# Stats -------------------------------------------------------------------
 
+fig.res <- res %>% group_by(POP, SP.95) %>% 
+      summarise(N = n()) %>% 
+      ggplot(aes(x = POP, y = N, fill = SP.95)) +
+      geom_bar(stat= "identity") + 
+      theme_bw()
 
-names(res) <- c("ID", "POP.assign", "fasciatus", "mentella_golf", "model")
-
-
-res %>% filter(model == "naiveBayes") %>% 
-        gather("fasciatus", "mentella_golf", key = "Species", value = "Prob") %>% 
-        left_join(assign.df %>% select(ID, POP)) %>% 
-        filter(Prob >= 0.97) %>% 
-        group_by(POP, model, Species) %>% 
-        summarise(N = n()) %>% View()
-
-library(corrplot)
-
-res %>% select(-POP.assign) %>% 
-   gather("fasciatus", "mentella_golf", key = "Species", value = "Prob") %>% 
-   spread(model, value = Prob) %>%  
-    left_join( assign.df %>% select(ID, POP)) %>% 
-   
-       filter(Species  == "fasciatus",
-              POP == "Maria",
-              naiveBayes >=0.97) %>% # View()
-   select(naiveBayes, randomForest, svm, tree) %>% 
-   plot()
-   
-
-
-names(res1) <- c("ID", "POP.assign", "fasciatus", "mentella_golf", "model")
-
-res.final <- res1 %>% mutate(POP.assign = ifelse(POP.assign == "pop.1", "fasciatus", "mentella_golf"),
-                                                 POP = ifelse(str_detect(ID, "Maria"), "Maria", "Repro"))
-res.final
-
-res.Maria <- res.final %>% filter(POP == "Maria",
-                                  mentella_golf >= 0.95) %>% 
-                           arrange(desc(mentella_golf)) %>% 
-                           transmute(ID, SP=POP.assign, Membership.prob = mentella_golf)
-res.Maria    
-
-write.csv2(res.Maria, "Resultats_Mentella_Maria_2019-06-06.csv", row.names = F)
-
-res.Maria2 <- res.final %>% filter(POP == "Maria") %>% 
-                            mutate(SP.95 = ifelse(POP.assign == "fasciatus" & fasciatus >= 0.95, "fasciatus",
-                                                  ifelse(POP.assign == "mentella_golf" & mentella_golf >= 0.95, "mentella_golf",
-                                                         "undetermined")) ) %>% 
-                            transmute(ID, SP.95, Prob.fasciatus = fasciatus, Prob.mentella_golf = mentella_golf)
-
-all.ID.Maria <- as.data.frame(read_excel(file.path("02_Data_to_Assign",assign.excel),  col_types = rep("text", 2 + length(locus) * 2))) %>% 
-                            select(ID) %>% 
-                            filter(str_detect(ID, "Maria"))
-
-res.Maria2 <- res.Maria2 %>% full_join(all.ID.Maria) %>% mutate(SP.95 = ifelse(is.na(SP.95),"undetermined", SP.95))    
-res.Maria2
-
-write.csv2(res.Maria2, "Resultats_Mentella_Maria_Complet_2019-06-11.csv", row.names = F)
-
-
-
-
-res.Caro <- res.final %>% filter(POP != "Maria") %>% 
-                          mutate(SP.95 = ifelse(POP.assign == "fasciatus" & fasciatus >= 0.95, "fasciatus",
-                                         ifelse(POP.assign == "mentella_golf" & mentella_golf >= 0.95, "mentella_golf",
-                                          "undetermined")) ) %>% 
-                          transmute(ID, SP.95, Prob.fasciatus = fasciatus, Prob.mentella_golf = mentella_golf)
-
-res.Caro                                  
-write.csv2(res.Caro, "Resultats_Sebastes_Caro_2019-06-06.csv", row.names = F)
-
+ggsave(plot = fig.res, filename = file.path(assign.dir, "Results_assignments.png"),
+       width = 7,
+       height = 4,
+       units = c("in"))
